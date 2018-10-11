cf8d914bed5763c4c850d3f6b97a51901c0bc2ac
GuoGuiRong <3095764372@qq.com>
2018-10-11 21:51:37 +0800

    [add] 第四篇:断路器（Hystrix）(Finchley版本)

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 [1] 第一篇： 服务的注册与发现Eureka(Finchley版本)
 
 [2] 第二篇: 服务消费者（rest+ribbon）(Finchley版本)
+
+[3] 第三篇: 服务消费者（Feign）(Finchley版本)
+
+[4] 第四篇:断路器（Hystrix）(Finchley版本)
+
+[5] 第五篇: 路由网关(zuul)(Finchley版本)
---
+++ serice-feign/src/main/java/com/ggr/sericefeign/service/impl/SchedualServiceHiHystric.java
@@ -0,0 +1,12 @@
+package com.ggr.sericefeign.service.impl;
+
+import com.ggr.sericefeign.service.SchedualServiceHi;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SchedualServiceHiHystric implements SchedualServiceHi {
+    @Override
+    public String sayHiFromClientOne(String name) {
+        return "sorry "+name;
+    }
+}
